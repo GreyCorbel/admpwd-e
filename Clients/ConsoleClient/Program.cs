c293fed8ffa0c75a3430a1acff48b0dd8275b3ce
Jiri Formacek <jirif@microsoft.com>
2015-10-12 08:48:10 +0200

    Initial version containing simple ConsoleClient application

---
+++ Clients/ConsoleClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AdmPwd.ServiceUtils;
+using System.ServiceModel;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(string.Format("Getting the password for computer {0}", args[0]));
+                AdmPwd.Types.PasswordInfo pwdData = PdsWrapper.GetPassword(forestName: string.Empty, ComputerName: args[0], IncludePasswordHistory: false);
+                Console.WriteLine(string.Format("Password: {0}", pwdData.Password));
+                Console.WriteLine(string.Format("Expires: {0}", pwdData.ExpirationTimestamp.ToString()));
+
+                Console.Write("Resetting password ... ");
+
+                PdsWrapper.ResetPassword(forestName: string.Empty, computerName: args[0], whenEffective: DateTime.Now);
+                Console.WriteLine("done");
+            }
+            catch (FaultException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
