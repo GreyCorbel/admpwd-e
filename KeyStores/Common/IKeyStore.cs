d443db7343c15426b166fa77fad88c768c8d9034
Jiří Formáček <jiri.formacek@hotmail.com>
2016-01-01 13:52:55 +0100

    Initial release of KeyStore based on Azure KeyVault Added source for AdmPwd.PDS.IKeyStore interface Added simple test app

---
+++ KeyStores/Common/IKeyStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AdmPwd.Service.KeyStore
+{
+    public interface IKeyStore
+    {
+        List<int> SupportedKeySizes { get; }
+
+        Dictionary<UInt32, string> PublicKeys { get; }
+
+        string Decrypt(UInt32 keyID, string EncryptedPwd);
+
+        UInt32 GenerateKeyPair(int KeySize);
+
+        string GetPublicKey(UInt32 KeyID);
+    }
+}
---
+++ KeyStores/TestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdmPwd.PDS.AzureKeyStore;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AzureKeyVaultStore store = new AzureKeyVaultStore();
+
+            var sizes = store.SupportedKeySizes;
+
+            
+        }
+    }
+}
