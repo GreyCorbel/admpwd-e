1d83c4d760045f7d85e381e5cdf30309da7d1bf5
JIRIF <jiri.formacek@greycorbel.com>
2016-11-14 09:38:46 +0100

    Updated AdmPwd.ServiceUtils.dll to match v7.5.0.1 of project Increased version to 7.5.0.1

--- Clients/SolutionItems/GlobalAssemblyInfo.cs
+++ Clients/SolutionItems/GlobalAssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("7.5.0.0")]
-[assembly: AssemblyFileVersion("7.5.0.0")]
+[assembly: AssemblyVersion("7.5.0.1")]
+[assembly: AssemblyFileVersion("7.5.0.1")]
 
 [assembly: AssemblyCompany("Jiri Formacek")]
 [assembly: AssemblyCopyright("")]
