d946b34650b9405b52373078aa2d4924457f3669
JIRIF <jiri.formacek@greycorbel.com>
2017-12-26 16:05:17 +0100

    Added simple CSharp implementation of RunAsAdmin tool - does not support local admin account Renamed CPlusPlus implementation of RunAsAdmin

---
+++ Clients/RunAsAdmin_Cs/Native.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace RunAsAdmin
+{
+    [Flags]
+    enum CreationFlags
+    {
+        CREATE_SUSPENDED = 0x00000004,
+        CREATE_NEW_CONSOLE = 0x00000010,
+        CREATE_NEW_PROCESS_GROUP = 0x00000200,
+        CREATE_UNICODE_ENVIRONMENT = 0x00000400,
+        CREATE_SEPARATE_WOW_VDM = 0x00000800,
+        CREATE_DEFAULT_ERROR_MODE = 0x04000000,
+        ABOVE_NORMAL_PRIORITY_CLASS = 0x00008000,
+    }
+
+    [Flags]
+    enum LogonFlags
+    {
+        LOGON_WITH_PROFILE = 0x00000001,
+        LOGON_NETCREDENTIALS_ONLY = 0x00000002
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public struct StartupInfo
+    {
+        public int cb;
+        public String reserved;
+        public String desktop;
+        public String title;
+        public int x;
+        public int y;
+        public int xSize;
+        public int ySize;
+        public int xCountChars;
+        public int yCountChars;
+        public int fillAttribute;
+        public int flags;
+        public UInt16 showWindow;
+        public UInt16 reserved2;
+        public byte reserved3;
+        public IntPtr stdInput;
+        public IntPtr stdOutput;
+        public IntPtr stdError;
+    }
+
+    internal struct ProcessInformation
+    {
+        public IntPtr process;
+        public IntPtr thread;
+        public int processId;
+        public int threadId;
+    }
+    class Native
+    {
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool CreateProcessWithLogonW(
+            String userName,
+            String domain,
+            String password,
+            UInt32 logonFlags,
+            String applicationName,
+            String commandLine,
+            UInt32 creationFlags,
+            UInt32 environment,
+            String currentDirectory,
+            ref StartupInfo startupInfo,
+            out ProcessInformation processInformation);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool GetExitCodeProcess(IntPtr process, ref UInt32 exitCode);
+
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        public static extern UInt32 WaitForSingleObject(IntPtr handle, UInt32 milliseconds);
+
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetStdHandle(IntPtr handle);
+
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        public static extern bool CloseHandle(IntPtr handle);
+        [DllImport("Kernel32.dll")]
+        public static extern int GetLastError();
+
+    }
+}
---
+++ Clients/RunAsAdmin_Cs/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using AdmPwd.PDSUtils;
+using AdmPwd.Types;
+
+namespace RunAsAdmin
+{
+    class Program
+    {
+        public const UInt32 Infinite = 0xffffffff;
+        public const Int32 Startf_UseStdHandles = 0x00000100;
+        public const Int32 StdOutputHandle = -11;
+        public const Int32 StdErrorHandle = -12;
+
+
+        static void Main(string[] args)
+        {
+            string adminAccountName=null;
+            string pathToExecutable=null;
+            string domainName = null;
+
+            LogonFlags dwLogonFlags = LogonFlags.LOGON_WITH_PROFILE;
+
+            foreach (string arg in args)
+            {
+                if (arg.StartsWith("/user:", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    adminAccountName = arg.Substring(6);
+                    continue;
+                }
+                if (arg.StartsWith("/path:", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    pathToExecutable = arg.Substring(6);
+                    continue;
+                }
+                if (arg.StartsWith("/noLocalProfile", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    dwLogonFlags = LogonFlags.LOGON_NETCREDENTIALS_ONLY;
+                    continue;
+                }
+                if (arg.StartsWith("/?", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    Usage();
+                    return;
+                }
+            }
+            if(adminAccountName==null || pathToExecutable==null)
+            {
+                Usage();
+                return;
+            }
+
+            if (adminAccountName.Contains("\\"))
+            {
+                string[] pairs = adminAccountName.Split('\\');
+                domainName = pairs[0];
+                adminAccountName = pairs[1];
+            }
+            try
+            {
+                PasswordInfo pwdInfo = PdsWrapper.GetManagedAccountPassword(null, adminAccountName, false);
+
+                StartupInfo si = new StartupInfo();
+                si.cb = Marshal.SizeOf(si);
+                ProcessInformation pi = new ProcessInformation();
+                bool rslt = Native.CreateProcessWithLogonW(adminAccountName, domainName, pwdInfo.Password, (uint)dwLogonFlags, null, pathToExecutable, (uint)(CreationFlags.CREATE_NEW_PROCESS_GROUP | CreationFlags.ABOVE_NORMAL_PRIORITY_CLASS), 0, null, ref si, out pi);
+                if (!rslt)
+                {
+                    throw new System.ComponentModel.Win32Exception(Native.GetLastError());
+                }
+            }
+            catch(PDSException ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
+            catch(System.ComponentModel.Win32Exception ex)
+            {
+                Console.WriteLine($"ERROR: Failed to start process, Win32 return code: {ex.NativeErrorCode.ToString("X2")}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("RunAsAdmin.exe /path:<path to executable to run> [/user:<name of admin account>] [/noLocalProfile]");
+            Console.WriteLine("");
+            Console.WriteLine("EXAMPLES:");
+            Console.WriteLine("Runs command prompt as domain account:");
+            Console.WriteLine("  RunAsAdmin /path:%SystemRoot%\\system32\\cmd.exe /user:myaccount@mydomain.com");
+            Console.WriteLine("");
+            Console.WriteLine("Runs command prompt as domain user without creating local profile and without caching the password on local machine:");
+            Console.WriteLine("  RunAsAdmin /path:%SystemRoot%\\system32\\cmd.exe /user:mydomain\\myaccount /noLocalProfile");
+            Console.WriteLine("");
+
+        }
+    }
+}
